ad17610d5c08694b3e9886610fa1c2efbc046099
yohhalley <yohhalley@gmail.com>
2015-01-20 15:43:10 -0600

    Submission of Assignment 2 for R Programming Course

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## MakeCacheMatrix: Receives a matrix, in order to assign it to 
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(M = matrix()) {
+  MInv <- NULL
+  set <- function(Y) {
+    M <<- Y
+    MInv <<- NULL
+    }
+  get <- function() M
+  setInv <- function(setMInv) MInv <<- setMInv
+  getInv <- function() MInv
+  list(set = set, get = get,
+             setInv = setInv,
+             getInv = getInv)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(M, ...) {
         ## Return a matrix that is the inverse of 'x'
+  MInv <- M$getInv()
+    if(!is.null(MInv)) {
+    message("getting cached data")
+    return(MInv)
+  }
+  
+  data <- M$get()
+  MInv <- solve(data)
+  M$setInv(MInv)
+  MInv
 }
